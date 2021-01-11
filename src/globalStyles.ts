53681a09a4966a5ed8c1717d25a3e49203d736ec
JosueDLA <josuedlavs@gmail.com>
2021-01-11 14:24:05 -0600

    Add styled components

---
+++ src/globalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        color: black;
+        background-color: white;
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import NavBar from "../components/Layout/NavBar/navBar";
+import GlobalStyle from "../globalStyles";
 
 const IndexPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <NavBar />
+    </React.Fragment>
+  );
 };
 
 export default IndexPage;
