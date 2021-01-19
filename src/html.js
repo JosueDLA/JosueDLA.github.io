fe6a0680bbf6f546fab6e1386503770a8f4dd02f
JosueDLA <josuedlavs@gmail.com>
2021-01-19 03:28:08 -0600

    Fix styled-components font warning

--- src/html.js
+++ src/html.js
@@ -11,6 +11,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500;600;700&display=swap"
+          rel="stylesheet"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
--- src/style/globalStyles.ts
+++ src/style/globalStyles.ts
@@ -3,8 +3,6 @@ import { DefaultTheme } from "./theme";
 import scrollBar from "./scrollBar";
 
 const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
-    /* @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500;600;700&display=swap'); */
-
     body{
         margin: 0;
         padding: 0;
