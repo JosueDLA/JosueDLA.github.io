8f65a69764cd92fd77d056f971ed5a0c73252c7b
JosueDLA <josuedlavs@gmail.com>
2021-01-22 02:55:14 -0600

    Add Card component

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  background: ${(t) => t.theme.bgSecondary};
+  max-width: 360px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  border-radius: 2rem;
+  box-shadow: 0px 1rem 1.5rem rgba(0, 0, 0, 0.5);
+  margin: 2rem 0;
+`;
+
+export const Banner = styled.div`
+  color: ${(t) => t.theme.textPrimary};
+  height: 10rem;
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+
+  svg {
+    background-color: black;
+    width: 10rem;
+    height: 10rem;
+    box-shadow: 0 0.5rem 1rem rgba(black, 0.3);
+    border-radius: 50%;
+    transition: transform 200ms cubic-bezier(0.18, 0.89, 0.32, 1.28);
+
+    &:hover {
+      transform: scale(1.3);
+    }
+  }
+`;
+
+export const CardTittle = styled.h2`
+  margin-top: 0;
+  text-align: center;
+`;
+
+export const CardDesc = styled.p`
+  text-align: justify;
+  padding: 0 2rem 2.5rem;
+`;
