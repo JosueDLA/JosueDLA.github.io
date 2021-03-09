4f337063b579d005b847f7aa059acde37fb49cc4
JosueDLA <josuedlavs@gmail.com>
2021-03-08 23:27:00 -0600

    Add AllPosts types

--- src/templates/allPosts.tsx
+++ src/templates/allPosts.tsx
@@ -4,8 +4,30 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout/Layout";
 
 export interface AllPostsProps {
-  pageContext: any;
-  data: any;
+  pageContext: IPageContext;
+  data: IData;
+}
+
+interface IData {
+  allMdx: {
+    edges: [
+      node: {
+        frontmatter: {
+          date: string;
+          excerpt: string;
+          slug: string;
+          title: string;
+        };
+      }
+    ];
+  };
+}
+
+interface IPageContext {
+  currentPage: number;
+  limit: number;
+  numPages: number;
+  skip: number;
 }
 
 const AllPosts: React.FC<AllPostsProps> = ({ pageContext, data }) => {
@@ -17,6 +39,9 @@ const AllPosts: React.FC<AllPostsProps> = ({ pageContext, data }) => {
     currentPage - 1 === 1 ? `${pagePath}` : `${pagePath}${currentPage - 1}`;
   const next = `${pagePath}${currentPage + 1}`;
   const posts = data.allMdx.edges;
+
+  console.log("pageContext :>> ", pageContext);
+  console.log("data :>> ", data);
 
   return (
     <Layout>
