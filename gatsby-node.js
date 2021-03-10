2ec51efabea6017d35b45d70a776bbfe6920ddc0
JosueDLA <josuedlavs@gmail.com>
2021-03-10 01:22:21 -0600

    Add function for creating posts programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,10 +12,11 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
 
 exports.createPages = async ({ actions, graphql }) => {
   // Define template
-  const blogPost = require.resolve("./src/templates/allPosts.tsx");
+  const blog = require.resolve("./src/templates/AllPosts.tsx");
+  const post = require.resolve("./src/templates/SinglePost.tsx");
 
   // Get all markdown post sorted by date
-  const { data } = await graphql(`
+  const result = await graphql(`
     query {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
@@ -30,20 +31,43 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `);
 
-  // Create paginated pages for post
-  const postPerPage = 6;
-  const numPages = Math.ceil(data.allMdx.edges.length / postPerPage);
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your blog posts`,
+      result.errors
+    );
+    return;
+  }
 
-  Array.from({ length: numPages }).forEach((_, i) => {
+  // Create paginated pages for post
+  const posts = result.data.allMdx.edges;
+
+  if (posts.length > 0) {
+    const postPerPage = 3;
+    const numPages = Math.ceil(posts.length / postPerPage);
+
+    Array.from({ length: numPages }).forEach((_, i) => {
+      actions.createPage({
+        path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+        component: blog,
+        context: {
+          limit: postPerPage,
+          skip: i * postPerPage,
+          numPages,
+          currentPage: i + 1,
+        },
+      });
+    });
+  }
+
+  // Create blog post
+  result.data.allMdx.edges.forEach((edge) => {
+    const slug = edge.node.frontmatter.slug;
+    const id = edge.node.id;
     actions.createPage({
-      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-      component: blogPost,
-      context: {
-        limit: postPerPage,
-        skip: i * postPerPage,
-        numPages,
-        currentPage: i + 1,
-      },
+      path: `/blog/${slug}`,
+      component: require.resolve(post),
+      context: { id },
     });
   });
 };
