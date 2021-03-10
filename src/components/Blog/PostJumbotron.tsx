c0e311af06733b8c9c65fc208820b2738e3a52ae
JosueDLA <josuedlavs@gmail.com>
2021-03-10 04:21:01 -0600

    Add blog post Jumbotron component

---
+++ src/components/Blog/PostJumbotron.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const PostJumbotron = styled.aside`
+  padding: 0;
+  margin: 0;
+  border: 0;
+  vertical-align: baseline;
+  width: 100%;
+  background-color: var(--bg-secondary);
+
+  header {
+    position: relative;
+    max-width: 640px;
+    padding: 35px 10px 30px 10px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 42;
+    font-weight: 700;
+  }
+`;
