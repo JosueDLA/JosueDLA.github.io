9f20cb6a65749fde5a5271becc347f32bef4d77f
JosueDLA <josuedlavs@gmail.com>
2021-06-28 23:03:33 -0600

    Update projects page size

--- src/components/Projects/ProjectsItems.tsx
+++ src/components/Projects/ProjectsItems.tsx
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 export const ProjectsWrapper = styled.section`
   background: var(--bg-primary);
-  height: 100%;
-  min-height: 100vh;
+  min-height: calc(100vh - 7rem);
 `;
