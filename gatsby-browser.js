bc85e3812c6dcf8fdfda84017197cedd5641b0c0
JosueDLA <josuedlavs@gmail.com>
2021-06-30 07:53:29 -0600

    Add syntas highlighting to more languages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,15 @@ import { MDXProvider } from "@mdx-js/react";
 import { preToCodeBlock } from "mdx-utils";
 import Table from "Src/components/Blog/Table";
 import Code from "Src/components/Blog/Code";
+import Prism from "prism-react-renderer/prism";
 import "Src/style/language.css";
+
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+
+require("prismjs/components/prism-bash");
+require("prismjs/components/prism-docker");
+require("prismjs/components/prism-csharp");
+require("prismjs/components/prism-powershell");
 
 const components = {
   table: Table,
