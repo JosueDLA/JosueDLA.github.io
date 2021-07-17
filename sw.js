c25b32dfca9bb0e404382b49f9a3629054b4c836
JosueDLA <josuedlavs@gmail.com>
2021-07-17 01:17:21 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3eda59d52783cc803ef1.js"
+    "url": "webpack-runtime-c42602ebcf7be649c8b8.js"
   },
   {
     "url": "framework-159c3f980b6b4151d570.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8447e7e4fb5665b1f9a0007bf12d02fc"
+    "revision": "33a65a13d99bb3a34742b1e9e7be5626"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8328be481434b8467bfd.js"
@@ -58,10 +58,10 @@ self.__precacheManifest = [
     "url": "6728d85a-5c7c714d0773ea6facd6.js"
   },
   {
-    "url": "0e99aa6ecfbaf03ccf621b14e86a3842a7a577b2-ee7b83597e1b3328a8c5.js"
+    "url": "0e99aa6ecfbaf03ccf621b14e86a3842a7a577b2-5a5528f8c4c968e599d1.js"
   },
   {
-    "url": "component---src-pages-index-tsx-8cbd6abc30b918665911.js"
+    "url": "component---src-pages-index-tsx-f5a07a891eb8ef8c3592.js"
   },
   {
     "url": "page-data/index/page-data.json",
@@ -77,13 +77,13 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0e2a866960c1623c64bb2945c4beac15"
+    "revision": "6e2ab92686b78c48e95f492182e43d1f"
   },
   {
-    "url": "3c73754e3071f37fff86ea024def9e2612f04d48-5f6b936453c1557db8d9.js"
+    "url": "3c73754e3071f37fff86ea024def9e2612f04d48-80572ae134897314e778.js"
   },
   {
-    "url": "component---src-templates-all-posts-tsx-3721a602a557fe3bd783.js"
+    "url": "component---src-templates-all-posts-tsx-565b36e7cd3f1a9cdd58.js"
   },
   {
     "url": "page-data/blog/2/page-data.json",
@@ -101,7 +101,7 @@ self.__precacheManifest = [
     "url": "0c428ae2-e49c5469f13807d67d03.js"
   },
   {
-    "url": "component---src-templates-single-post-tsx-5fa4e1a7eb3ee18a1aeb.js"
+    "url": "component---src-templates-single-post-tsx-61635b7f6ac4a8322e4e.js"
   },
   {
     "url": "page-data/blog/basic-odoo-module/page-data.json",
@@ -148,7 +148,7 @@ self.__precacheManifest = [
     "revision": "00b757fad39ba0a9966d42711feb1bff"
   },
   {
-    "url": "component---src-templates-all-projects-tsx-4224cb75291126964c1e.js"
+    "url": "component---src-templates-all-projects-tsx-408630a6c02626fc3a2d.js"
   },
   {
     "url": "page-data/projects/2/page-data.json",
